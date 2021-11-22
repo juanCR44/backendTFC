ab7b2ca48e1578a6f67839474491266fa1858059
juanCR44 <juancastror.1235@gmail.com>
2021-11-21 22:34:44 -0500

    update

--- main/main.go
+++ main/main.go
@@ -1,51 +1,99 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"net/http"
-	"os"
-	"strings"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	router := gin.Default()
-	router.Use(cors.Default())
+type Importacion struct {
+	Anio     string `json:"anio"`
+	Mes      string `json:"mes"`
+	Producto string `json:"producto"`
+	Peso     string `json:"peso"`
+}
 
-	router.POST("/upload", func(c *gin.Context) {
-		fmt.Println(c.ContentType())
+var importaciones []Importacion
 
-		file, _, err := c.Request.FormFile("file")
+func handleRequests() {
+	fmt.Print("eheheiuehui")
+	mux := http.NewServeMux()
 
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
-			return
-		}
+	mux.HandleFunc("/upload", cargarImportaciones)
+	mux.HandleFunc("/importaciones", mostrarImportaciones)
 
-		csvlines, err := csv.NewReader(file).ReadAll()
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
 
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
-			return
-		}
+func mostrarImportaciones(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "application/json")
+	jsonBytes, _ := json.MarshalIndent(importaciones, "", " ")
+	io.WriteString(resp, string(jsonBytes))
+}
 
-		fmt.Print(csvlines)
-
-	})
-
-	bufferIn := bufio.NewReader(os.Stdin)
-	fmt.Print("Ingrese el puerto remoto: ")
-	puerto, _ := bufferIn.ReadString('\n')
-	puerto = strings.TrimSpace(puerto)
-	remotehost := fmt.Sprintf("localhost:%s", puerto)
-	con, _ := net.Dial("tcp", remotehost)
+func conexionCluster(importaciones []Importacion) {
+	remotehost := "localhost:9003"
+	con, err := net.Dial("tcp", remotehost)
+	fmt.Print(err, " ERRORORO")
 	defer con.Close()
-	fmt.Fprintln(con, 35)
+	fmt.Fprintln(con, importaciones)
+	fmt.Fprintln(con, "")
+}
 
-	router.Run(":8000")
+func cargarImportaciones(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	resp.Header().Set("Access-Control-Allow-Credentials", "true")
+	resp.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	resp.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	if req.Method == "POST" {
+		fs, _, err := req.FormFile("file")
+		if err != nil {
+			http.Error(resp, "Error", http.StatusBadRequest)
+		}
+
+		fmt.Printf("Correcto")
+		if err != nil {
+			fmt.Printf("get form err: %s", err)
+		}
+		defer fs.Close()
+		r := csv.NewReader(fs)
+
+		for {
+			row, err := r.Read()
+			if err != nil && err != io.EOF {
+				fmt.Printf("can not read, err is %+v", err)
+			}
+			if err == io.EOF {
+				break
+			}
+			importacion := Importacion{
+				Anio:     row[0],
+				Mes:      row[1],
+				Producto: row[2],
+				Peso:     row[3],
+			}
+
+			importaciones = append(importaciones, importacion)
+		}
+
+		//fmt.Print(importaciones)
+		conexionCluster(importaciones)
+		resp.Header().Set("Content-Type", "application/json")
+
+		jsonBytes, _ := json.MarshalIndent(importaciones, "", " ")
+		io.WriteString(resp, string(jsonBytes))
+
+	} else {
+		http.Error(resp, "Método inválido", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func main() {
+	handleRequests()
 }
